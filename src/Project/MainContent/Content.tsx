e11e86dfba0d151e00175da5da0fee00bb82c9ae
Ali <aliimottaghiii@gmail.com>
2025-03-15 15:11:00 +0330

    resize the Content

--- src/Project/MainContent/Content.tsx
+++ src/Project/MainContent/Content.tsx
@@ -12,9 +12,9 @@ export default function Content({dataTransfer} : any) {
                     <li>Drug Name : {dataTransfer.Drug_Dosage}</li>
                     <li>Drug Name : {dataTransfer.Drug_count_order}</li>
                     <li>Drug Name : {dataTransfer.Drug_Country_maker}</li>
-                    {/* <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis reprehenderit debitis, modi necessitatibus consequatur qui magni cum ducimus! Nesciunt ea excepturi consequatur aut voluptas quod facilis animi quia quae odit.</li>
+                    <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis reprehenderit debitis, modi necessitatibus consequatur qui magni cum ducimus! Nesciunt ea excepturi consequatur aut voluptas quod facilis animi quia quae odit.</li>
                     <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis tempora, atque qui, non officia magni ipsa quidem enim, voluptas quasi odit harum nemo! Quos sapiente error eligendi atque, sit corrupti!</li>
-                    <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis sed praesentium ut at, impedit fuga, nemo facere unde veniam odio asperiores nihil sequi voluptatibus corporis expedita! Necessitatibus eius repudiandae dolores?</li> */}
+                    <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis sed praesentium ut at, impedit fuga, nemo facere unde veniam odio asperiores nihil sequi voluptatibus corporis expedita! Necessitatibus eius repudiandae dolores?</li>
                 </ul>
             </div>
         </>
