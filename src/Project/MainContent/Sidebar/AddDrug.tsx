5d632080315fc519d88ad779520739a3754e85e5
Ali <aliimottaghiii@gmail.com>
2025-05-05 22:24:43 +0330

    AddDrug.tsx modified / added onchange function and attribute and update the inputs of add drug

--- src/Project/MainContent/Sidebar/AddDrug.tsx
+++ src/Project/MainContent/Sidebar/AddDrug.tsx
@@ -1,37 +1,76 @@
 import { useState } from "react"
 
+//type of the drugAtrib and updater function 
+interface DrugAtribType {
+    name : string,
+    Dosage : number | undefined,
+    Country : string,
+    Distribution_Company : string,
+    ExpireDate : string
+}
+
 export default function AddDrug() {
-    const [drugAtrib , setDrugAtrib] = useState({
+    const [drugAtrib , setDrugAtrib] = useState<DrugAtribType>({
         name : "",
-        Dosage : "",
+        Dosage : undefined,
         Country : "",
         Distribution_Company : "",
         ExpireDate : ""
     })
     
+    let handleChangeDrugName = (e : any) => {
+        setDrugAtrib({
+            ...drugAtrib,
+            name : e.target.value
+        })
+    }
+    let handleChangeDrugDosage = (e : any) => {
+        setDrugAtrib({
+            ...drugAtrib,
+            Dosage : e.target.value
+        })
+    }
+    let handleChangeDrugCountry = (e : any) => {
+        setDrugAtrib({
+            ...drugAtrib,
+            Country : e.target.value
+        })
+    }
+    let handleChangeDrugDistributionCompany = (e : any) => {
+        setDrugAtrib({
+            ...drugAtrib,
+            Distribution_Company : e.target.value
+        })
+    }
+    let handleChangeDrugExpireDate = (e : any) => {
+        setDrugAtrib({
+            ...drugAtrib,
+            ExpireDate : e.target.value
+        })
+    }
     return (
         <>
              <h2>Add Drug</h2>
              <form action="nameDrug" className="AddDrugFrom">
                 <label htmlFor="nameDrug" className="DrugName">
-                    Drug Name : 
-                    <input type="text" id="nameDrug" className="inputForm" placeholder="Fluoxetine"/>
+                    Drug Name :
+                    <input type="text" value={drugAtrib.name} onChange={handleChangeDrugName} id="nameDrug" className="inputForm" placeholder="Fluoxetine"/>
                 </label>
                 <label htmlFor="dosage" className="Dosage">
                     Dosage : 
-                    <input type="text" id="dosage" className="inputForm" placeholder="120 mg"/>
+                    <input type="number" value={drugAtrib.Dosage} onChange={handleChangeDrugDosage} id="dosage" className="inputForm" placeholder="120 mg"/>
                 </label>
                 <label htmlFor="country" className="Country">
                     Country : 
-                    <input type="text" id="country" className="inputForm" placeholder="Germany"/>
+                    <input type="text" value={drugAtrib.Country} onChange={handleChangeDrugCountry} id="country" className="inputForm" placeholder="Germany"/>
                 </label>
                 <label htmlFor="distribution" className="DistCompany">
                     Distribution Company : 
-                    <input type="text" id="distribution" className="inputForm" placeholder="Razi Company"/>
+                    <input type="text" value={drugAtrib.Distribution_Company} onChange={handleChangeDrugDistributionCompany} id="distribution" className="inputForm" placeholder="Razi Company"/>
                 </label>
                 <label htmlFor="expireDate" className="ExpireDate">
                     Expire Date : 
-                    <input type="date" id="expireDate" className="inputForm" placeholder="2025/23/4"/>
+                    <input type="date" value={drugAtrib.ExpireDate} onChange={handleChangeDrugExpireDate} id="expireDate" className="inputForm" placeholder="2025/23/4"/>
                 </label>
              </form>
         </>
