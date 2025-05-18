f7e624f9cb3a484bcab44004b49e04b807cfd27c
Ali-Raven <alimateeee@gmail.com>
2025-05-18 16:15:23 +0330

    add first picture of mine and do some style changing like background color

--- src/Project/MainContent/Sidebar/home.tsx
+++ src/Project/MainContent/Sidebar/home.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import firstPageImage from '../../../../photo_2025-05-13_18-54-02.jpg'
 
 export default function Home() {
     return (
         <React.Fragment>
             <div className="container-HomePage">
                 <h2 className='titleHomePage'>Future of PIS is in your hands</h2>
-                <div className='mainTextHomePage'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore beatae ipsa quasi atque commodi modi consectetur totam accusamus, ipsum sequi nemo optio accusantium libero porro quis dolore eos corporis doloribus?</div>
+                <div className='mainTextHomePage'>L libero porro quis dolore eos corporis doloribus?</div>
+                <img src={firstPageImage} alt="first page of PIS Poster" width={1000} height={800}/>
             </div>
         </React.Fragment>
     )
